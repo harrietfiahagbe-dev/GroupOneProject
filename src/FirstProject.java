44c42ce62d617a1596a51c96bb3c437f86810622
harrietfiahagbe-dev <harrietfiahagbe@gmail.com>
2025-09-25 08:23:56 +0000

    This is our first code!

--- src/FirstProject.java
+++ src/FirstProject.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class FirstProject {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        final double POUND = 0.45359237;
+        final double INCH = 0.0254;
+
+        System.out.println("Enter your height: ");
+       double user_Height =  input.nextDouble();
+
+        System.out.println("Enter your weight: ");
+        double user_Weight = input.nextDouble();
+
+        double weight_kg = user_Weight * POUND;
+        double height_m = user_Height * INCH;
+
+        double BMI = weight_kg/(height_m*height_m);
+
+        System.out.println("Your BMI is:  " + BMI);
+
+
+
 
     }
 
